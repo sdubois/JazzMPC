eb7635e955664bcb69886362b87c9f0c1f497fc6
CSH-eventsdb <esw1726@rit.edu>
2011-03-19 17:05:44 -0700

    Now uses POST to pass arguments

--- jazzmpc.js
+++ jazzmpc.js
@@ -1,8 +1,9 @@
 var routes = new require('routes').Router();
 var http = require('http');
+var fs = require('fs');
 var mpdSocket = require('mpdsocket');
+var qs = require('querystring');
 var io = require("socket.io");
-var jazzmpc;
 
 var mpd = new mpdSocket('192.168.189.130',6600);
 
@@ -13,95 +14,93 @@ mpd.on('connect',function() {
 	                      'clear','playlistinfo','shuffle','listplaylists','listall',
 	                      'listallinfo','lsinfo','update','rescan','disableoutput',
 	                      'enableoutput','outputs','commands','notcommands',
-	                      'tagtypes','urlhandlers','decoders','channels','readmessages'];
-	
-	var simpleArgFuncList = ['consume','crossfade','mixrampdb','mixrampdelay',
-	                         'random','repeat','setvol','single','replay_gain_mode',
-	                         'pause','play','playid','password','add','delete','deleteid',
-	                         'playlistinfo','playlistid','plchanges','plchangesposid',
-	                         'shuffle','listplaylist','listplaylistinfo','load',
-	                         'playlistclear','rm','save','listall','listallinfo',
-	                         'lsinfo','update','rescan','subscribe','unsubscribe'];
-
-	//other functions
-	mpdfn('seek',true,'/mpd/seek/:a/:b');
-	mpdfn('seekid',true,'/mpd/seekid/:a/:b');
-	mpdfn('addid',true,'/mpd/addid/:a/:b?');
-	mpdfn('swap',true,'/mpd/swap/:a/:b');
-	mpdfn('swapid',true,'/mpd/swap/:a/:b');
-	mpdfn('playlistid',true,'/mpd/playlistid/:a/:b');
-	mpdfn('playlistdelete',true,'/mpd/playlistdelete/:a/:b');
-	mpdfn('playlistmove',true,'/mpd/playlistmove/:a/:b/:c');
-	mpdfn('rename',true,'/mpd/rename/:a/:b');
-	mpdfn('count',true,'/mpd/count/:a/:b');
-	mpdfn('find',true,'/mpd/find/:a/:b');
-	mpdfn('findadd',true,'/mpd/findadd/:a/:b');
-	mpdfn('list',true,'/mpd/list/:a/:b?');
-	mpdfn('search',true,'/mpd/search/:a/:b');
-	mpdfn('sendmessage',true,'/mpd/sendmessage/:a/:b');
+	                      'tagtypes','urlhandlers','decoders','channels','readmessages',
+	                      'consume','crossfade','mixrampdb','mixrampdelay',
+	                      'random','repeat','setvol','single','replay_gain_mode',
+	                      'pause','play','playid','password','add','delete','deleteid',
+	                      'playlistinfo','playlistid','plchanges','plchangesposid',
+	                      'shuffle','listplaylist','listplaylistinfo','load',
+	                      'playlistclear','rm','save','listall','listallinfo',
+	                      'lsinfo','update','rescan','subscribe','unsubscribe',
+	                      'seek','seekid','addid','swap','swapid','playlistid',
+	                      'playlistdelete','playlistmove','rename','count','find',
+	                      'findadd','list','search','sendmessage'];
 
 	for (var i in simpleFuncList) {
-		mpdfn(simpleFuncList[i],false);
+		mpdfn(simpleFuncList[i]);
 	}
-	
-	for (var i in simpleArgFuncList) {
-		mpdfn(simpleArgFuncList[i],true,'/mpd/' + simpleArgFuncList[i] + '/:option');
-	}
-	
-	function _simpleWrite(req) {
-	    return function() {
+
+	function sendFile(path) {
 		var res = this;
-	        mpd.send(req,function(r) {
-		   if (typeof(r) == 'object') {
-		      res.end(JSON.stringify(r) + "\n");
-		   } else {
-		      res.end(r + "\n");
-		   }
+		fs.readFile(__dirname + path,function(err,data) {
+			if (err) {
+				res.writeHead(404);
+				res.end("Not found\n");
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data,'utf-8');
+				res.end();
+			}
 		});
-	    }
 	}
-	
-	function _simpleWriteArg(req) {
+
+	function _simpleWrite(req) {
 	    return function(param) {
 		var res = this;
-		var query = req + " ";
-		for (var i in param) {
-			query += param[i] + " ";
+		if (!(param)) {
+		        mpd.send(req,function(r) {
+			   if (typeof(r) == 'object') {
+			      res.end(JSON.stringify(r) + "\n");
+			   } else {
+			      res.end(r + "\n");
+			   }
+			});
+		} else {
+			var query = req + " ";
+			for (var i in param) {
+				query += decodeURI(param[i]) + " ";
+			}
+	
+			mpd.send(query,function(r) {
+			   if (typeof(r) == 'object') {
+			      res.end(JSON.stringify(r) + "\n");
+			   } else {
+			      res.end(r + "\n");
+			   }
+			});
 		}
-
-		mpd.send(query,function(r) {
-		   if (typeof(r) == 'object') {
-		      res.end(JSON.stringify(r) + "\n");
-		   } else {
-		      res.end(r + "\n");
-		   }
-		});
 	    }
 	}
 	
-	function mpdfn(req,arg,route) {
+	function mpdfn(req) {
 		var evalString = "var mpd_" + req + ' = _simpleWrite';
-		if (arg) evalString += 'Arg';
 		evalString += '("' + req + '");';
 		eval(evalString);
 	
-		if (!(route)) route = "/mpd/" + req;
+		var route = "/mpd/" + req;
 		routes.addRoute(route,eval("mpd_" + req));
 	}
 	
 	//initialize server
-	jazzmpc = http.createServer(function(req,res) {
+	var jazzmpc = http.createServer(function(req,res) {
 		console.log(req.url);
 		var routed = routes.match(req.url);
 		if (routed) {
 			//acceptable request
 			res.writeHead(200, {'Content-Type': 'text/plain'});
-			if (JSON.stringify(routed.params) != "{}") {
+			if (req.method == "POST") {
 				//we have parameters we need to pass
-				routed.fn.call(res,routed.params);
-			} else {
-				routed.fn.call(res);
+				var postData = "";
+				req.on('data',function(data) {
+					postData += data;
+				});
+				req.on('end',function() {
+					var postParams = qs.parse(postData);
+					routed.fn.call(res,postParams);
+				});
 			}
+		} else if (req.url == "/index.html") {
+			sendFile.call(res,"/index.html");
 		} else {
 			//unacceptable request
 			res.writeHead(404);
